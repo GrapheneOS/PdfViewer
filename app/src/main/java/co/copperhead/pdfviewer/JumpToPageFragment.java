e59e9efb56bc315aaf8c1487e105ae61f9a3db9a
Tommy-Geenexus <roboter972@gmail.com>
2017-02-21 20:05:51 +0100

    Handle number picker in fragment

---
+++ app/src/main/java/co/copperhead/pdfviewer/JumpToPageFragment.java
@@ -0,0 +1,60 @@
+package co.copperhead.pdfviewer;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.NumberPicker;
+
+public class JumpToPageFragment extends DialogFragment {
+    private final static String STATE_PICKER_CUR = "picker_cur";
+    private final static String STATE_PICKER_MIN = "picker_min";
+    private final static String STATE_PICKER_MAX = "picker_max";
+
+    private NumberPicker mPicker;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            mPicker.setMinValue(savedInstanceState.getInt(STATE_PICKER_MIN));
+            mPicker.setMaxValue(savedInstanceState.getInt(STATE_PICKER_MAX));
+            mPicker.setValue(savedInstanceState.getInt(STATE_PICKER_CUR));
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mPicker = new NumberPicker(getActivity());
+        mPicker.setMinValue(1);
+        mPicker.setMaxValue(((PdfViewer)getActivity()).mNumPages);
+        mPicker.setValue(((PdfViewer)getActivity()).mPage);
+
+        final FrameLayout layout = new FrameLayout(getActivity());
+        layout.addView(mPicker, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                Gravity.CENTER));
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(layout)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ((PdfViewer)getActivity()).positiveButtonRenderPage(mPicker.getValue());
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .create();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(STATE_PICKER_MIN, mPicker.getMinValue());
+        outState.putInt(STATE_PICKER_MAX, mPicker.getMaxValue());
+        outState.putInt(STATE_PICKER_CUR, mPicker.getValue());
+    }
+}
