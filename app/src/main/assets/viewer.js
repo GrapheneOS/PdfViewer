cf7adb817578230d8b682b2c6d4a2524db25a895
Daniel Micay <danielmicay@gmail.com>
2018-03-12 16:13:42 -0400

    implement Least Recently Used (LRU) rendering cache

--- app/src/main/assets/viewer.js
+++ app/src/main/assets/viewer.js
@@ -6,10 +6,13 @@ let renderPending = false;
 let renderPendingLazy = false;
 let canvas = document.getElementById('content');
 let zoomLevel = 100;
-const textLayerDiv = document.getElementById("text");
+let textLayerDiv = document.getElementById("text");
 const zoomLevels = [50, 75, 100, 125, 150];
 let renderTask = null;
 let textLayerRenderTask = null;
+
+const cache = [];
+const maxCached = 5;
 
 function maybeRenderNextPage() {
     if (renderPending) {
@@ -23,13 +26,34 @@ function maybeRenderNextPage() {
 
 function renderPage(lazy) {
     pageRendering = true;
-    pdfDoc.getPage(channel.getPage()).then(function(page) {
-        let last;
-        while (last = textLayerDiv.lastChild) {
-            textLayerDiv.removeChild(last);
-        }
 
-        const newZoomLevel = zoomLevels[channel.getZoomLevel()];
+    function finishRendering() {
+        pageRendering = false;
+        maybeRenderNextPage();
+    }
+
+    const pageNumber = channel.getPage();
+    const newZoomLevel = zoomLevels[channel.getZoomLevel()];
+    for (let i = 0; i < cache.length; i++) {
+        let cached = cache[i];
+        if (cached.pageNumber == pageNumber && cached.zoomLevel == newZoomLevel) {
+            canvas.replaceWith(cached.canvas);
+            canvas = cached.canvas;
+            textLayerDiv.replaceWith(cached.textLayerDiv);
+            textLayerDiv = cached.textLayerDiv;
+
+            cache.splice(i, 1);
+            cache.push(cached);
+
+            finishRendering();
+            return;
+        }
+    }
+
+    pdfDoc.getPage(pageNumber).then(function(page) {
+        const newTextLayerDiv = textLayerDiv.cloneNode();
+        textLayerDiv.replaceWith(newTextLayerDiv);
+        textLayerDiv = newTextLayerDiv;
 
         const newCanvas = document.createElement("canvas");
         const viewport = page.getViewport(newZoomLevel / 100)
@@ -57,11 +81,6 @@ function renderPage(lazy) {
             viewport: viewport
         });
 
-        function finishRendering() {
-            pageRendering = false;
-            maybeRenderNextPage();
-        }
-
         renderTask.then(function() {
             if (maybeRenderNextPage()) {
                 return;
@@ -85,6 +104,15 @@ function renderPage(lazy) {
                 });
                 textLayerRenderTask.promise.then(function() {
                     textLayerDiv.appendChild(textLayerFrag);
+                    if (cache.length == maxCached) {
+                        cache.shift()
+                    }
+                    cache.push({
+                        pageNumber: pageNumber,
+                        zoomLevel: zoomLevel,
+                        canvas: canvas,
+                        textLayerDiv: textLayerDiv
+                    });
                     finishRendering();
                 }).catch(finishRendering);
             }).catch(finishRendering);
