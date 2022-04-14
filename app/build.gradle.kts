ca6d987c238d16b6d16b726a914f3f16b7753e47
Patryk Mis <patryk.mis@mykolab.com>
2022-04-14 12:52:26 +0200

    Add build config for pl

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
         versionCode = 13
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
+        resourceConfigurations.add("pl")
     }
 
     buildTypes {
