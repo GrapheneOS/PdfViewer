db98371c9d18156667ada1ebbc10d50e8b9a3d50
Daniel Micay <danielmicay@gmail.com>
2022-05-06 07:00:16 -0400

    increment version to 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "app.grapheneos.pdfviewer"
         minSdk = 26
         targetSdk = 32
-        versionCode = 13
+        versionCode = 14
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
     }
