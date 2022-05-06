ea9a57fabee34072f596a73d4813ced56ebfba85
Pratyush <codelab@pratyush.dev>
2022-05-06 22:42:13 +0530

    Improve password UI/UX

---
+++ app/src/main/java/app/grapheneos/pdfviewer/viewModel/PasswordStatus.kt
@@ -0,0 +1,27 @@
+package app.grapheneos.pdfviewer.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PasswordStatus : ViewModel() {
+
+    enum class Status {
+        MissingPassword,
+        InvalidPassword,
+        Validated
+    }
+
+    val status: MutableLiveData<Status> = MutableLiveData(Status.MissingPassword)
+
+    fun passwordMissing() {
+        status.postValue(Status.MissingPassword)
+    }
+
+    fun invalid() {
+        status.postValue(Status.InvalidPassword)
+    }
+
+    fun validated() {
+        status.postValue(Status.Validated)
+    }
+}
