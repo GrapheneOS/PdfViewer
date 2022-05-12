18761b3e4a93b14e298a5f25e657774b8eb70cb5
Pratyush <codelab@pratyush.dev>
2022-05-12 16:53:55 +0530

    replace deprecated api uses

--- app/src/main/java/app/grapheneos/pdfviewer/PdfViewer.java
+++ app/src/main/java/app/grapheneos/pdfviewer/PdfViewer.java
@@ -44,6 +44,7 @@ import app.grapheneos.pdfviewer.databinding.PdfviewerBinding;
 import app.grapheneos.pdfviewer.fragment.DocumentPropertiesFragment;
 import app.grapheneos.pdfviewer.fragment.PasswordPromptFragment;
 import app.grapheneos.pdfviewer.fragment.JumpToPageFragment;
+import app.grapheneos.pdfviewer.ktx.ViewKt;
 import app.grapheneos.pdfviewer.loader.DocumentPropertiesLoader;
 import app.grapheneos.pdfviewer.viewModel.PasswordStatus;
 
@@ -331,8 +332,7 @@ public class PdfViewer extends AppCompatActivity implements LoaderManager.Loader
                         if (mUri != null) {
                             binding.webview.evaluateJavascript("isTextSelected()", selection -> {
                                 if (!Boolean.parseBoolean(selection)) {
-                                    if ((getWindow().getDecorView().getSystemUiVisibility() &
-                                            View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                                    if (getSupportActionBar().isShowing()) {
                                         hideSystemUi();
                                     } else {
                                         showSystemUi();
@@ -569,21 +569,12 @@ public class PdfViewer extends AppCompatActivity implements LoaderManager.Loader
     }
 
     private void showSystemUi() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        ViewKt.showSystemUi(binding.getRoot());
         getSupportActionBar().show();
     }
 
     private void hideSystemUi() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_IMMERSIVE);
+        ViewKt.hideSystemUi(binding.getRoot());
         getSupportActionBar().hide();
     }
 
