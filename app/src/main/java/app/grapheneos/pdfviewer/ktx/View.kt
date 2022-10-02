0bfd1b1d127b08b5739d7f417fd2386ef34a6722
Pratyush <codelab@pratyush.dev>
2022-10-02 18:42:24 +0530

    replace deprecated api uses

--- app/src/main/java/app/grapheneos/pdfviewer/ktx/View.kt
+++ app/src/main/java/app/grapheneos/pdfviewer/ktx/View.kt
@@ -1,19 +1,20 @@
 package app.grapheneos.pdfviewer.ktx
 
 import android.view.View
-import androidx.core.view.ViewCompat
+import android.view.Window
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
 private val systemBars = WindowInsetsCompat.Type.statusBars()
 
-fun View.hideSystemUi() {
-    val controller = ViewCompat.getWindowInsetsController(this) ?: return
+fun View.hideSystemUi(window: Window) {
+    val controller = WindowCompat.getInsetsController(window, this)
     controller.systemBarsBehavior =
         WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     controller.hide(systemBars)
 }
 
-fun View.showSystemUi() {
-    ViewCompat.getWindowInsetsController(this)?.show(systemBars)
+fun View.showSystemUi(window: Window) {
+    WindowCompat.getInsetsController(window, this).show(systemBars)
 }
