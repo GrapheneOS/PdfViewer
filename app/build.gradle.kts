7f59118453016796608019dcbc5445488a45bc54
Daniel Micay <danielmicay@gmail.com>
2023-01-13 19:54:54 -0500

    update AndroidX appcompat library to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
 }
