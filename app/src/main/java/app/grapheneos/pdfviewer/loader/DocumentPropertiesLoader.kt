acb0a09fe06921802a65fc6ef3c403d6cfc3cd70
Pratyush <codelab@pratyush.dev>
2023-04-07 17:37:50 +0530

    rewrite DocumentPropertiesLoader in kotlin

---
+++ app/src/main/java/app/grapheneos/pdfviewer/loader/DocumentPropertiesLoader.kt
@@ -0,0 +1,113 @@
+package app.grapheneos.pdfviewer.loader
+
+import android.content.Context
+import android.graphics.Typeface
+import android.net.Uri
+import android.provider.OpenableColumns
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.util.Log
+import app.grapheneos.pdfviewer.R
+import app.grapheneos.pdfviewer.Utils
+import org.json.JSONException
+
+class DocumentPropertiesLoader(
+    private val context: Context,
+    private val properties: String,
+    private val numPages: Int,
+    private val mUri: Uri
+) {
+
+    fun loadAsList(): List<CharSequence> {
+        return load().map { item ->
+            val name = context.getString(item.key.nameResource)
+            val value = item.value
+
+            SpannableStringBuilder()
+                .append(name)
+                .append(":\n")
+                .append(value)
+                .apply {
+                    setSpan(
+                        StyleSpan(Typeface.BOLD),
+                        0,
+                        name.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                }
+        }
+    }
+
+    fun load(): Map<DocumentProperty, String> {
+        val result = mutableMapOf<DocumentProperty, String>()
+        result.addFileProperties()
+        result.addPageSizeProperty()
+        result.addPDFJsProperties()
+        return result
+    }
+
+    private fun MutableMap<DocumentProperty, String>.addPageSizeProperty() {
+        this[DocumentProperty.Pages] = java.lang.String.valueOf(numPages)
+    }
+
+    private fun MutableMap<DocumentProperty, String>.addFileProperties() {
+        putAll(getFileProperties())
+    }
+
+    private fun MutableMap<DocumentProperty, String>.addPDFJsProperties() {
+        putAll(getPDFJsProperties())
+    }
+
+    private fun getPDFJsProperties(): Map<DocumentProperty, String> {
+        return try {
+            PDFJsPropertiesToDocumentPropertyConverter(
+                properties,
+                context.getString(R.string.document_properties_invalid_date),
+                parseExceptionListener = { parseException, value ->
+                    Log.w(
+                        DocumentPropertiesAsyncTaskLoader.TAG,
+                        "${parseException.message} for $value at offset: ${parseException.errorOffset}"
+                    )
+                }
+            ).convert()
+        } catch (e: JSONException) {
+            Log.w(
+                DocumentPropertiesAsyncTaskLoader.TAG,
+                "invalid properties"
+            )
+            emptyMap()
+        }
+    }
+
+    private fun getFileProperties(): Map<DocumentProperty, String> {
+        val collections = mutableMapOf<DocumentProperty, String>()
+        val proj = arrayOf(
+            OpenableColumns.DISPLAY_NAME,
+            OpenableColumns.SIZE
+        )
+
+        context.contentResolver.query(
+            mUri,
+            proj,
+            null,
+            null
+        )?.use { cursor ->
+            cursor.moveToFirst()
+            val indexName: Int = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+
+            if (indexName >= 0) {
+                collections[DocumentProperty.FileName] = cursor.getString(indexName)
+            }
+
+            val indexSize: Int = cursor.getColumnIndex(OpenableColumns.SIZE)
+            if (indexSize >= 0) {
+                val fileSize: Long = cursor.getString(indexSize).toLong()
+                collections[DocumentProperty.FileSize] = Utils.parseFileSize(fileSize)
+            }
+        }
+        return collections
+    }
+
+
+}
---
+++ app/src/main/java/app/grapheneos/pdfviewer/loader/PDFJsPropertiesToDocumentPropertyConverter.kt
@@ -0,0 +1,49 @@
+package app.grapheneos.pdfviewer.loader
+
+import app.grapheneos.pdfviewer.Utils
+import org.json.JSONException
+import org.json.JSONObject
+import java.text.ParseException
+import kotlin.jvm.Throws
+
+class PDFJsPropertiesToDocumentPropertyConverter(
+    private val properties: String,
+    private val propertyInvalidDate: String,
+    private val parseExceptionListener: (e: ParseException, value: String) -> Unit
+) {
+
+    @Throws(JSONException::class)
+    fun convert(): Map<DocumentProperty, String> {
+        val result = mutableMapOf<DocumentProperty, String>()
+
+        val json = JSONObject(properties)
+        addJsonProperties(json, result)
+        return result
+    }
+
+    private fun addJsonProperties(
+        json: JSONObject,
+        collections: MutableMap<DocumentProperty, String>
+    ) {
+        for (documentProperty in DocumentProperty.values()) {
+            val key = documentProperty.key
+            if (key.isEmpty()) continue
+            val value = json.optString(key, DEFAULT_VALUE)
+            collections[documentProperty] = prettify(documentProperty, value)
+        }
+    }
+
+    private fun prettify(property: DocumentProperty, value: String): String {
+        if (value != DEFAULT_VALUE && property.isDate) {
+            return try {
+                Utils.parseDate(value)
+            } catch (parseException: ParseException) {
+                parseExceptionListener.invoke(parseException, value)
+                propertyInvalidDate
+            }
+        }
+        return value
+    }
+
+
+}
