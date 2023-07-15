5900e381e85b56d2cb507d7886ff5469f2afd319
heapifyman <274236+heapifyman@users.noreply.github.com>
2023-07-15 18:12:37 +0200

    add navigating back and forth in PDF by swiping right or left

--- app/src/main/java/app/grapheneos/pdfviewer/GestureHelper.java
+++ app/src/main/java/app/grapheneos/pdfviewer/GestureHelper.java
@@ -7,6 +7,8 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
 /*
     The GestureHelper present a simple gesture api for the PdfViewer
 */
@@ -14,6 +16,10 @@ import android.view.View;
 class GestureHelper {
     public interface GestureListener {
         boolean onTapUp();
+
+        void onSwipeLeft();
+        void onSwipeRight();
+
         // Can be replaced with ratio when supported
         void onZoomIn(float value);
         void onZoomOut(float value);
@@ -28,6 +34,31 @@ class GestureHelper {
                     @Override
                     public boolean onSingleTapUp(MotionEvent motionEvent) {
                         return listener.onTapUp();
+                    }
+
+                    // inspired by https://www.geeksforgeeks.org/how-to-detect-swipe-direction-in-android/
+                    @Override
+                    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+                        final int swipeThreshold = 100;
+                        final int swipeVelocityThreshold = 100;
+
+                        final float diffX = e2.getX() - e1.getX();
+                        final float absDiffX = Math.abs(diffX);
+                        final float diffY = e2.getY() - e1.getY();
+                        final float absDiffY = Math.abs(diffY);
+
+                        if (absDiffX > absDiffY // only handle horizontal swipe
+                                && absDiffX > swipeThreshold
+                                && Math.abs(velocityX) > swipeVelocityThreshold) {
+                            if (diffX > 0) {
+                                listener.onSwipeRight();
+                            } else {
+                                listener.onSwipeLeft();
+                            }
+                            return true;
+                        }
+
+                        return false;
                     }
                 });
 
