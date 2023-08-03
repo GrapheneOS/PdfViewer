56071e7c53e7917921413f5a480746db29021565
octocorvus <admin@octocorvus.dev>
2023-08-03 08:37:56 +0000

    build: hook viewer build tasks to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.apache.tools.ant.taskdefs.condition.Os
 import java.util.Properties
 import java.io.FileInputStream
 
@@ -93,4 +94,31 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+}
+
+fun getCommand(command: String, winExt: String = "cmd"): String {
+    return if (Os.isFamily(Os.FAMILY_WINDOWS)) "$command.$winExt" else command
+}
+
+val npmSetup = tasks.register("npmSetup", Exec::class) {
+    workingDir = rootDir
+    commandLine(getCommand("npm"), "ci", "--ignore-scripts")
+}
+
+val processStatic = tasks.register("processStatic", Exec::class) {
+    workingDir = rootDir
+    dependsOn(npmSetup)
+    commandLine(getCommand("node", "exe"), "process_static.js")
+}
+
+val cleanStatic = tasks.register("cleanStatic", Delete::class) {
+    delete("src/main/assets/viewer", "src/debug/assets/viewer")
+}
+
+tasks.preBuild {
+    dependsOn(processStatic)
+}
+
+tasks.clean {
+    dependsOn(cleanStatic)
 }
