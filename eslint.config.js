c9eb4225457540b6e794e67713a1c06f8233055d
Daniel Micay <daniel.micay@grapheneos.org>
2024-04-22 17:41:10 -0400

    move to new eslint configuration format

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                channel: "readonly",
+                ...globals.browser
+            },
+            ecmaVersion: 2022,
+            sourceType: "module"
+        },
+        rules: {
+            indent: ["error", 4],
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+            "no-unused-vars": ["error", {caughtErrors: "none"}],
+            "no-var": ["error"]
+        },
+
+    },
+    {
+        ignores: [
+            "app/build/",
+            "app/src/*/assets/viewer",
+            "releases/"
+        ]
+    }
+];
