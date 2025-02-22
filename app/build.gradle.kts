5cfd041a2b2a550948f79e2d592a0bc96609d709
Daniel Micay <daniel.micay@grapheneos.org>
2025-02-22 14:04:32 -0500

    increment version to 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         applicationId = "app.grapheneos.pdfviewer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
+        versionCode = 25
         versionName = versionCode.toString()
     }
 
