6346fcc322a9bfb6a1c96b1624c4953588144b26
Daniel Micay <daniel.micay@grapheneos.org>
2025-09-04 13:57:28 -0400

    update Material Components library to 1.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
 }
 
 fun getCommand(command: String, winExt: String = "cmd"): String {
