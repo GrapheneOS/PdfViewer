1abfea12acdee0ab022fd8e4b3b087d77aaab78a
ggtlvkma356 <265052551+ggtlvkma356@users.noreply.github.com>
2026-03-30 18:27:13 +0200

    Allow jumping to previous/next page with horizontal swipe gesture
    - Add swipe gesture controls for jumping to previous/next page
    - Prevent page change during zoom

--- app/src/main/java/app/grapheneos/pdfviewer/GestureHelper.java
+++ app/src/main/java/app/grapheneos/pdfviewer/GestureHelper.java
@@ -7,6 +7,11 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /*
     The GestureHelper present a simple gesture api for the PdfViewer
 */
@@ -14,6 +19,7 @@ import android.view.View;
 class GestureHelper {
     public interface GestureListener {
         boolean onTapUp();
+        boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY);
         void onZoom(float scaleFactor, float focusX, float focusY);
         void onZoomEnd();
     }
@@ -21,18 +27,12 @@ class GestureHelper {
     @SuppressLint("ClickableViewAccessibility")
     static void attach(Context context, View gestureView, GestureListener listener) {
 
-        final GestureDetector detector = new GestureDetector(context,
-                new GestureDetector.SimpleOnGestureListener() {
-                    @Override
-                    public boolean onSingleTapUp(MotionEvent motionEvent) {
-                        return listener.onTapUp();
-                    }
-                });
+        AtomicBoolean wasScaling = new AtomicBoolean(false);
 
         final ScaleGestureDetector scaleDetector = new ScaleGestureDetector(context,
                 new ScaleGestureDetector.SimpleOnScaleGestureListener() {
                     @Override
-                    public boolean onScale(ScaleGestureDetector detector) {
+                    public boolean onScale(@NonNull ScaleGestureDetector detector) {
                         listener.onZoom(detector.getScaleFactor(), detector.getFocusX(),
                                 detector.getFocusY());
 
@@ -40,14 +40,44 @@ class GestureHelper {
                     }
 
                     @Override
-                    public void onScaleEnd(ScaleGestureDetector detector) {
+                    public void onScaleEnd(@NonNull ScaleGestureDetector detector) {
                         listener.onZoomEnd();
                     }
                 });
 
+        final GestureDetector detector = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
+                        return listener.onTapUp();
+                    }
+
+                    @Override
+                    public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2,
+                                           float velocityX, float velocityY) {
+                        if (wasScaling.get()) {
+                            return false;
+                        }
+
+                        return listener.onFling(e1, e2, velocityX, velocityY);
+                    }
+                });
+
         gestureView.setOnTouchListener((view, motionEvent) -> {
+            int action = motionEvent.getActionMasked();
+
+            if (action == MotionEvent.ACTION_DOWN) {
+                wasScaling.set(false);
+            }
+
             detector.onTouchEvent(motionEvent);
             scaleDetector.onTouchEvent(motionEvent);
+
+            // Check after scaleDetector processes the event
+            if (scaleDetector.isInProgress()) {
+                wasScaling.set(true);
+            }
+
             return false;
         });
     }
